3f31e89436665348ed7e78544a343896506b5a09
Igor Becker <igor_nyu@hotmail.com>
2023-10-11 11:26:45 -0300

    :check:

--- lua/igor/lsp.lua
+++ lua/igor/lsp.lua
@@ -8,6 +8,11 @@ nvim_lsp.denols.setup {
     on_attach = on_attach,
     root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc")
 }
+nvim_lsp.tsserver.setup {
+    on_attach = on_attach,
+    root_dir = nvim_lsp.util.root_pattern("package.json"),
+    single_file_support = false
+}
 
 lsp.ensure_installed({
     'tsserver',
