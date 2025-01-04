4a4a83186db4d3396d9bcba374d60fbe084e7497
Igor Becker <igor_nyu@hotmail.com>
2025-01-04 16:43:40 -0300

    feat: adding autocomplete

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -12,7 +12,12 @@ return {
                         { path = "${3rd}/luv/library", words = { "vim%.uv" } },
                     },
                 },
-            }
+            },
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
         },
         config = function()
             -- server configs
@@ -37,16 +42,58 @@ return {
                 filetypes = { 'c', 'cpp' }
             }
 
-            config.ts_ls.setup {
+            config.tsserver.setup {
                 cmd = { 'typescript-language-server', '--stdio' },
                 filetypes = { 'typescriptreact', 'typescript', 'javascript' },
-                root_markers = { 'package.json' }
+                root_markers = { 'package.json', 'deno.json' }
             }
 
             -- config.lexical.setup {
             --     cmd = { 'lexical' },
             --     filetypes = { 'ex', 'exs' }
             -- }
+
+            local cmp = require('cmp')
+            -- local cmp_lsp = require("cmp_nvim_lsp")
+            -- local capabilities = vim.tbl_deep_extend(
+            -- "force",
+            -- {},
+            -- vim.lsp.protocol.make_client_capabilities(),
+            -- cmp_lsp.default_capabilities())
+
+            local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+                    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                }, {
+                    { name = 'buffer' },
+                })
+            })
+
+            vim.diagnostic.config({
+                -- update_in_insert = true,
+                float = {
+                    focusable = false,
+                    style = "minimal",
+                    border = "rounded",
+                    source = true,
+                    header = "",
+                    prefix = "",
+                },
+            })
 
             -- keymap configs
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
